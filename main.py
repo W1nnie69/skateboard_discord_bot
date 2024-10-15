997123942ec148540af7c9e4bdefccb4ef106666
W1nnie69 <danizye@gmail.com>
2024-10-15 23:32:28 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import config
+import os
+
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), application_id = 1295760755613765715)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
+
+    # Sync commands
+    print("Commands have been synced.")
+
+
+bot.run(config.token)
